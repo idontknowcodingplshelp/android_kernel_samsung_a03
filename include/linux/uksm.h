2edf0d092b3a61cc8592b99b496d5a5bfcb04941
idkcodingplshelp <behaiquy@gmail.com>
2025-02-02 16:26:58 +0700

    fix this too

--- include/linux/uksm.h
+++ include/linux/uksm.h
@@ -17,7 +17,7 @@
 #include <linux/sched.h>
 
 extern unsigned long zero_pfn;
-extern unsigned long uksm_zero_pfn __read_mostly;
+extern unsigned long uksm_zero_pfn;
 extern struct page *empty_uksm_zero_page;
 
 /* must be done before linked to mm */
